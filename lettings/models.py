3ce581c1a6036f65b1e16c134da2ec71e1377ebb
Didier Dardevet <didier.dardevet@gmail.com>
2021-12-06 09:31:26 +0100

    Renames Letting and profile tables with migrations

--- lettings/models.py
+++ lettings/models.py
@@ -12,7 +12,7 @@ class Address(models.Model):
 
     class Meta:
         verbose_name_plural = "Address"
-        db_table = "oc_lettings_site_address"
+        # db_table = "oc_lettings_site_address"
 
     def __str__(self):
         return f'{self.number} {self.street}'
@@ -22,8 +22,8 @@ class Letting(models.Model):
     title = models.CharField(max_length=256)
     address = models.OneToOneField(Address, on_delete=models.CASCADE)
 
-    class Meta:
-        db_table = 'oc_lettings_site_letting'
+    # class Meta:
+    #     db_table = 'oc_lettings_site_letting'
 
     def __str__(self):
         return self.title
--- profiles/models.py
+++ profiles/models.py
@@ -7,8 +7,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
-    class Meta:
-        db_table = "oc_lettings_site_profile"
+    # class Meta:
+    #     db_table = "oc_lettings_site_profile"
 
     def __str__(self):
         return self.user.username
