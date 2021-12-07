84b1e7b4786dd9a7f0058c93caedc5e6c247d9ce
Didier Dardevet <didier.dardevet@gmail.com>
2021-12-07 10:37:59 +0100

    add test lettings-index

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,2 +1,23 @@
-def test_dummy():
-    assert 1
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_index_views(client):
+    url = reverse('lettings:index')
+    print(url)
+    response = client.get(url)
+    expected_content = b"<title>"
+    assert response.status_code == 200
+    assert expected_content in response.content
+
+
+@pytest.mark.django_db
+def test_letting_views(client):
+    url = reverse('lettings:letting', kwargs={'letting_id': 1})
+    print(url)
+    response = client.get(url)
+    expected_content = b"<title>"
+    assert response.status_code == 200
+    assert expected_content in response.content
+
