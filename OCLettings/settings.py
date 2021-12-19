0e96c9ae667f95f211ac1e8292f4f14858df5f6f
Didier Dardevet <didier.dardevet@gmail.com>
2021-12-19 17:26:11 +0100

    Add Sentry

--- OCLettings/settings.py
+++ OCLettings/settings.py
@@ -5,7 +5,7 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_sdk.init(
-    dsn="https://48f4c7ecf96048bbaa46feec9d3aa100@o1094342.ingest.sentry.io/6113345",
+    dsn=os.environ['SENTRY_DSN'],
     integrations=[DjangoIntegration()],
 
     # Set traces_sample_rate to 1.0 to capture 100%
--- dockerfile
+++ dockerfile
@@ -1,7 +1,6 @@
 FROM python:3.9
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED 1
-ENV DJANGO_SECRET_KEY fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s
 
 run mkdir OCLettings-app
 WORKDIR /OCLettings-app
