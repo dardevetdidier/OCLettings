748f39e8e108ca1e7ca2cdf90c3a6df4479f2ff6
Didier Dardevet <didier.dardevet@gmail.com>
2021-12-19 17:53:37 +0100

    Add Sentry

--- OCLettings/settings.py
+++ OCLettings/settings.py
@@ -30,7 +30,7 @@ sentry_sdk.init(
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
